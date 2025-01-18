ac65d6d52f8a5710e31a3d34b58eeaeaa8aec6bd
zhulinyv <zhulinyv2005@outlook.com>
2025-01-18 20:36:53 +0800

    :tada: 首次提交

---
+++ __init__.py
@@ -0,0 +1,101 @@
+from pathlib import Path
+
+import gradio as gr
+
+from plugins.t2i.sanp_plugin_stories_to_images.utils import README, generate
+from src.text2image_nsfw import return_resolution
+from utils.env import env
+from utils.utils import NOISE_SCHEDULE, RESOLUTION, SAMPLER, open_folder, read_json
+
+webui_language = read_json(f"./files/languages/{env.webui_lang}/webui.json")
+
+
+def plugin():
+    with gr.Tab("推文生图工具"):
+        open_plugin_directory = gr.Button("打开插件目录")
+        generate_button = gr.Button("开始生成")
+        excel_path = gr.Textbox(label="Excel 文件路径")
+        images_number = gr.Slider(1, 20, 5, step=1, label="每段 tag 生成图片的数量")
+        gr.Markdown("<hr>")
+        sti_negative = gr.Textbox(label="负面提示词", lines=3)
+        with gr.Row():
+            sti_resolution = gr.Dropdown(
+                RESOLUTION,
+                value=("832x1216" if env.img_size == -1 else "{}x{}".format((env.img_size)[0], (env.img_size)[1])),
+                label=webui_language["t2i"]["resolution"],
+            )
+            sti_width = gr.Textbox(
+                value=(env.img_size)[0] if env.img_size != -1 else "832",
+                label=webui_language["t2i"]["width"],
+            )
+            sti_height = gr.Textbox(
+                value=(env.img_size)[1] if env.img_size != -1 else "1216",
+                label=webui_language["t2i"]["height"],
+            )
+            sti_resolution.change(
+                return_resolution,
+                sti_resolution,
+                outputs=[sti_width, sti_height],
+                show_progress="hidden",
+            )
+        with gr.Row():
+            sti_scale = gr.Slider(
+                minimum=0,
+                maximum=10,
+                value=env.scale,
+                step=0.1,
+                label=webui_language["t2i"]["scale"],
+            )
+            sti_steps = gr.Slider(minimum=0, maximum=50, value=env.steps, step=1, label=webui_language["t2i"]["steps"])
+        with gr.Row():
+            sti_sampler = gr.Dropdown(
+                SAMPLER,
+                value=env.sampler,
+                label=webui_language["t2i"]["sampler"],
+            )
+            sti_noise_schedule = gr.Dropdown(
+                NOISE_SCHEDULE,
+                value=env.noise_schedule,
+                label=webui_language["t2i"]["noise_schedule"],
+            )
+        with gr.Row():
+            sti_sm = gr.Checkbox(
+                value=env.sm if env.model != "nai-diffusion-4-curated-preview" else False,
+                label="sm",
+                visible=True if env.model != "nai-diffusion-4-curated-preview" else False,
+            )
+            sti_sm_dyn = gr.Checkbox(
+                value=env.sm_dyn if env.model != "nai-diffusion-4-curated-preview" else False,
+                label=webui_language["t2i"]["smdyn"],
+                visible=True if env.model != "nai-diffusion-4-curated-preview" else False,
+            )
+            sti_variety = gr.Checkbox(
+                value=env.variety if env.model != "nai-diffusion-4-curated-preview" else False,
+                label="variety",
+                visible=True if env.model != "nai-diffusion-4-curated-preview" else False,
+            )
+            sti_decrisp = gr.Checkbox(value=env.decrisp, label="decrisp")
+        opt_info = gr.Textbox(label="输出信息")
+        gr.Markdown(README)
+        open_plugin_directory.click(
+            open_folder, inputs=gr.Textbox(Path("./plugins/t2i/sanp_plugin_stories_to_images"), visible=False)
+        )
+        generate_button.click(
+            generate,
+            inputs=[
+                excel_path,
+                images_number,
+                sti_negative,
+                sti_width,
+                sti_height,
+                sti_scale,
+                sti_steps,
+                sti_sampler,
+                sti_noise_schedule,
+                sti_sm,
+                sti_sm_dyn,
+                sti_variety,
+                sti_decrisp,
+            ],
+            outputs=opt_info,
+        )
---
+++ utils.py
@@ -0,0 +1,112 @@
+import os
+import random
+
+import openpyxl
+import openpyxl.cell
+from openpyxl.drawing.image import Image
+from openpyxl.styles import Alignment
+
+from utils.env import env
+from utils.jsondata import json_for_t2i
+from utils.utils import generate_image, logger, return_x64, save_image, sleep_for_cool
+
+README = """## 使用说明
+
+使用前, 请打开插件目录将 "脚本文件示例.xlsx" 文件复制一份到任意位置.
+
+然后打开复制的文件, 将要依次生成的 tag 填入 TAG 列, 保存并关闭该文件.
+
+推文列可以不填, 为以后对接 GPT 做准备用的(实现通过推文生成 TAG 再生成图片).
+
+回到 webui 插件页面, 设置每条 tag 生成的数量, 以及生图参数, 点击开始生成即可.
+
+"""
+
+
+if not os.path.exists("./plugins/t2i/sanp_plugin_stories_to_images/脚本文件示例.xlsx"):
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.row_dimensions[1].height = 50
+    for row in range(2, 999):
+        ws.row_dimensions[row].height = 300
+    for col in [f"{chr(letter)}" for letter in range(ord("A"), ord("Z") + 1)]:
+        ws.column_dimensions[col].width = 40
+    ws.append(
+        [
+            "推文",
+            "TAG",
+            "图片",
+        ]
+    )
+    alignment = Alignment(horizontal="center", vertical="center", wrapText=True)
+    for row in ws.iter_rows():
+        for cell in row:
+            cell.alignment = alignment
+    wb.save("./plugins/t2i/sanp_plugin_stories_to_images/脚本文件示例.xlsx")
+
+
+def generate(
+    excel_path,
+    images_number,
+    sti_negative,
+    sti_width,
+    sti_height,
+    sti_scale,
+    sti_steps,
+    sti_sampler,
+    sti_noise_schedule,
+    sti_sm,
+    sti_sm_dyn,
+    sti_variety,
+    sti_decrisp,
+):
+    workbook = openpyxl.load_workbook(excel_path)
+    sheet = workbook["Sheet"]
+    col_num = 2
+    row_num = ord("C")
+    positive = sheet[f"B{col_num}"].value
+    while positive is not None:
+        row_num = ord("C")
+        for _ in range(images_number):
+            saved_path = "寄"
+            while saved_path == "寄":
+                json_for_t2i["input"] = positive
+                json_for_t2i["parameters"]["width"] = return_x64(int(sti_width))
+                json_for_t2i["parameters"]["height"] = return_x64(int(sti_height))
+                json_for_t2i["parameters"]["scale"] = sti_scale
+                json_for_t2i["parameters"]["sampler"] = sti_sampler
+                json_for_t2i["parameters"]["steps"] = sti_steps
+                if env.model != "nai-diffusion-4-curated-preview":
+                    json_for_t2i["parameters"]["sm"] = sti_sm if sti_sampler != "ddim_v3" else False
+                    json_for_t2i["parameters"]["sm_dyn"] = sti_sm_dyn if sti_sm and sti_sampler != "ddim_v3" else False
+                    json_for_t2i["parameters"]["skip_cfg_above_sigma"] = 19 if sti_variety else None
+                json_for_t2i["parameters"]["dynamic_thresholding"] = sti_decrisp
+                if sti_sampler != "ddim_v3":
+                    json_for_t2i["parameters"]["noise_schedule"] = sti_noise_schedule
+                seed = random.randint(1000000000, 9999999999)
+                json_for_t2i["parameters"]["seed"] = seed
+                json_for_t2i["parameters"]["negative_prompt"] = sti_negative
+
+                logger.debug(json_for_t2i)
+
+                saved_path = save_image(generate_image(json_for_t2i), "t2i", seed, "None", "None")
+
+            image = Image(saved_path)
+            w = image.width
+            h = image.height
+            image.width, image.height = 260, int(260 / w * h)
+            sheet.add_image(image, "{}{}".format(chr(row_num), col_num))
+
+            sleep_for_cool(env.t2i_cool_time - 3, env.t2i_cool_time + 3)
+
+            row_num += 1
+        col_num += 1
+        positive = sheet[f"B{col_num}"].value
+
+    alignment = Alignment(horizontal="center", vertical="center", wrapText=True)
+    for row in sheet.iter_rows():
+        for cell in row:
+            cell.alignment = alignment
+
+    workbook.save(excel_path)
+    return "处理完成!"
